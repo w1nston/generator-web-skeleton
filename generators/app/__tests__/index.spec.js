2df1110e38ab55f5ef0171fe8f6d25288cdc6805
w1nston <wallander.jonas@gmail.com>
2016-05-20 21:31:23 +0200

    Refactor generator tests to not create a directory, but instead use a temporary directory.

--- generators/app/__tests__/index.spec.js
+++ generators/app/__tests__/index.spec.js
@@ -1,72 +1,73 @@
-describe('generator', function () {
+describe('generator', function() {
   var assert = require('yeoman-assert');
   var helpers = require('yeoman-test');
   var path = require('path');
 
-  before(function () {
-    return helpers.run(path.join(__dirname, '../../app'))             // Run the generator from the app directory
-      .inDir(path.join(__dirname, './temporarily-generated-project')) // Place generated files in a tmp directory
+  before(function() {
+    return helpers
+      .run(path.join(__dirname, '../../app'))
+      .inTmpDir()
       .toPromise();
   });
 
-  describe('createFileStructure', function () {
-    it('generates an app directory', function () {
+  describe('createFileStructure', function() {
+    it('generates an app directory', function() {
       assert.file('app');
     });
 
-    it('generates a utils directory', function () {
+    it('generates a utils directory', function() {
       assert.file('utils');
     });
 
-    describe('for actions', function () {
-      it('generates a source directory', function () {
+    describe('for actions', function() {
+      it('generates a source directory', function() {
         assert.file('app/actions');
       });
 
-      it('generates a test directory', function () {
+      it('generates a test directory', function() {
         assert.file('app/actions/__tests__');
       });
     });
 
-    describe('for constants', function () {
-      it('generates a source directory', function () {
+    describe('for constants', function() {
+      it('generates a source directory', function() {
         assert.file('app/constants');
       });
     });
 
-    describe('for containers', function () {
-      it('generates a source directory', function () {
+    describe('for containers', function() {
+      it('generates a source directory', function() {
         assert.file('app/containers');
       });
 
-      it('generates a test directory', function () {
+      it('generates a test directory', function() {
         assert.file('app/containers/__tests__');
       });
     });
 
-    describe('for components', function () {
-      it('generates a source directory', function () {
+    describe('for components', function() {
+      it('generates a source directory', function() {
         assert.file('app/components');
       });
 
-      it('generates a test directory', function () {
+      it('generates a test directory', function() {
         assert.file('app/components/__tests__');
       });
     });
 
-    describe('for reducers', function () {
-      it('generates a source directory', function () {
+    describe('for reducers', function() {
+      it('generates a source directory', function() {
         assert.file('app/reducers');
       });
 
-      it('generates a test directory', function () {
+      it('generates a test directory', function() {
         assert.file('app/reducers/__tests__');
       });
     });
   });
 
-  describe('generateFiles', function () {
-    it('copies .babelrc from templates', function () {
+  describe('generateFiles', function() {
+    it('copies .babelrc from templates', function() {
       assert.file('./.babelrc');
       // TODO assert files are the same...
     });
@@ -107,13 +108,13 @@ describe('generator', function () {
     });
   });
 
-  describe('installNpmDependencies', function () {
+  describe('installNpmDependencies', function() {
     it('calls npmInstall with flag save set to true', function() {
       // TODO Use spy to see that this.npmInstall is called with correct dependencies
     });
   });
 
-  describe('installNpmDevDependencies', function () {
+  describe('installNpmDevDependencies', function() {
     it('calls npmInstall with flag saveDev set to true', function() {
       // TODO Use spy to see that this.npmInstall is called with correct dependencies
     });
